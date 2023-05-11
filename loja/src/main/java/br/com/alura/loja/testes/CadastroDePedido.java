881435f85b0ed1f5267d21e740a1baaee170e259
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-05-11 09:58:46 -0300

    criando pedidoDao

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -1,7 +1,43 @@
 package br.com.alura.loja.testes;
 
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.PedidoDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.*;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
 public class CadastroDePedido {
     public static void main(String[] args) {
-        
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        Produto produto = produtoDao.buscarPorId(1l);
+
+        Cliente cliente = new Cliente("Rodrigo", "123456");
+        Pedido pedido = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(10, pedido, produto));
+
+        PedidoDao pedidoDao = new PedidoDao(em);
+        pedidoDao.cadastrar(pedido);
+    }
+
+    private static void cadastrarProduto() {
+        Categoria celulares = new Categoria("CELULARES");
+        Produto celular = new Produto("Iphone 14PRO", "256GB preto", new BigDecimal(4000), celulares);
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+
+        em.getTransaction().begin();
+
+        categoriaDao.cadastrar(celulares);
+        produtoDao.cadastrar(celular);
+
+        em.getTransaction().commit();
+        em.close();
     }
 }
