7c1e7f8f445d3fa740a5402c4c8b07bfe30aca35
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-05-11 10:03:06 -0300

    clienteDao

---
+++ loja/src/main/java/br/com/alura/loja/dao/ClienteDao.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja.dao;
+
+import br.com.alura.loja.modelo.Cliente;
+
+import javax.persistence.EntityManager;
+
+public class ClienteDao {
+    private EntityManager em;
+
+    public ClienteDao (EntityManager em) {
+        this.em = em;
+    }
+    public void Cadastrar(Cliente cliente) {
+        this.em.persist(cliente);
+    }
+}
