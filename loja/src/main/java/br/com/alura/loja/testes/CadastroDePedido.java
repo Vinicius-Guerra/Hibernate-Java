9252e2d01926d8c2704009f08d81ad1fb9f07f66
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-05-11 10:08:24 -0300

    Update CadastroDePedido.java

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDePedido.java
@@ -1,6 +1,7 @@
 package br.com.alura.loja.testes;
 
 import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ClienteDao;
 import br.com.alura.loja.dao.PedidoDao;
 import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.*;
@@ -11,14 +12,14 @@ import java.math.BigDecimal;
 
 public class CadastroDePedido {
     public static void main(String[] args) {
-        cadastrarProduto();
+        popularBancoDeDados();
         EntityManager em = JPAUtil.getEntityManager();
         ProdutoDao produtoDao = new ProdutoDao(em);
         Produto produto = produtoDao.buscarPorId(1l);
 
         em.getTransaction().begin();
 
-        Cliente cliente = new Cliente("Rodrigo", "123456");
+
         Pedido pedido = new Pedido(cliente);
         pedido.adicionarItem(new ItemPedido(10, pedido, produto));
 
@@ -28,18 +29,22 @@ public class CadastroDePedido {
         em.getTransaction().commit();
     }
 
-    private static void cadastrarProduto() {
+    private static void popularBancoDeDados() {
         Categoria celulares = new Categoria("CELULARES");
         Produto celular = new Produto("Iphone 14PRO", "256GB preto", new BigDecimal(4000), celulares);
+
+        Cliente cliente = new Cliente("Rodrigo", "123456");
 
         EntityManager em = JPAUtil.getEntityManager();
         ProdutoDao produtoDao = new ProdutoDao(em);
         CategoriaDao categoriaDao = new CategoriaDao(em);
+        ClienteDao clienteDao = new ClienteDao(em);
 
         em.getTransaction().begin();
 
         categoriaDao.cadastrar(celulares);
         produtoDao.cadastrar(celular);
+        clienteDao.cadastrar(cliente);
 
         em.getTransaction().commit();
         em.close();
